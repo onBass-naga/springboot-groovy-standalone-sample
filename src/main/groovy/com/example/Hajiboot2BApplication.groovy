a75300d0dc1d90b6cfe2278597ff8a41e9ba6fa4
naga <naga@area-b.com>
2016-10-15 08:49:25 +0900

    はじめてのSpring Boot 改訂版 CommandLineRunnerでDI可能にする

--- src/main/groovy/com/example/Hajiboot2BApplication.groovy
+++ src/main/groovy/com/example/Hajiboot2BApplication.groovy
@@ -1,18 +1,32 @@
 package com.example
 
-import com.example.app.Frontend
+import com.example.app.Argument
+import com.example.app.ArgumentResolver
+import com.example.app.Calculator
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.ApplicationContext
 
 @SpringBootApplication
-class Hajiboot2BApplication {
+class Hajiboot2BApplication implements CommandLineRunner {
+
+	@Autowired
+	ArgumentResolver argumentResolver
+
+	@Autowired
+	Calculator calculator
+
+	@Override
+	void run(String... args) throws Exception {
+		print("Enter 2 numbers like a b : ")
+		Argument argument = argumentResolver.resolve(System.in)
+		int result = calculator.calc(argument.a, argument.b)
+
+		println("result = ${result}")
+	}
 
 	static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(
-				Hajiboot2BApplication, args)
-
-		Frontend frontend = context.getBean(Frontend.class)
-		frontend.run()
+		SpringApplication.run(Hajiboot2BApplication.class, args)
 	}
 }
