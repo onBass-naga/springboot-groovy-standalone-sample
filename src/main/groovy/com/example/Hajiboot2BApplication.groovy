b30012362b2744fb1ca843e60629b23169c381f0
naga <naga@area-b.com>
2016-10-15 11:37:29 +0900

    RepositoryにてDBアクセスする

--- src/main/groovy/com/example/Hajiboot2BApplication.groovy
+++ src/main/groovy/com/example/Hajiboot2BApplication.groovy
@@ -1,57 +1,26 @@
 package com.example
 
 import com.example.domain.Customer
+import com.example.service.CustomerService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.jdbc.core.RowMapper
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
-import org.springframework.jdbc.core.namedparam.SqlParameterSource
 
 @SpringBootApplication
 class Hajiboot2BApplication implements CommandLineRunner {
 
-//	@Autowired
-//	CustomerService customerService
-//
-//	@Override
-//	void run(String... args) throws Exception {
-//		customerService.save(new Customer(1, 'Nobita', 'Nobi'))
-//		customerService.save(new Customer(2, 'Takeshi', 'Goda'))
-//		customerService.save(new Customer(3, 'Suneo', 'Honekawa'))
-//
-//		customerService.findAll().each { println(it) }
-//	}
-
 	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate
+	CustomerService customerService
 
 	@Override
 	void run(String... args) throws Exception {
 
-		String sql = '''
-			SELECT
-				id, first_name, last_name
-			FROM
-				customers
-			WHERE
-				id = :id
-		'''
+		def created = customerService.save(
+				new Customer(null, 'Hidetoshi', 'Dekisugi'))
+		println("${created} is created!")
 
-		SqlParameterSource param = new MapSqlParameterSource()
-			.addValue("id", 2)
-		Customer result = jdbcTemplate.queryForObject(sql, param, {
-			rs, rowNum ->
-					return new Customer(
-							rs.getInt('id'),
-							rs.getString('first_name'),
-							rs.getString('last_name'))
-				} as RowMapper<Customer>
-		)
-
-		println(result)
+		customerService.findAll().each { println(it) }
 	}
 
 	static void main(String[] args) {
