9a554faae02db32ffd3d8bc9941ed530441ff58c
naga <naga@area-b.com>
2016-10-15 12:27:26 +0900

    データアクセスにJPAを使用するよう変更

--- src/main/groovy/com/example/domain/Customer.groovy
+++ src/main/groovy/com/example/domain/Customer.groovy
@@ -2,9 +2,24 @@ package com.example.domain
 
 import groovy.transform.Canonical
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Table
+
 @Canonical
+@Entity
+@Table(name = "customers")
 class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue
     Integer id
+
+    @Column(nullable = false)
     String firstName
+
+    @Column(nullable = false)
     String lastName
 }
