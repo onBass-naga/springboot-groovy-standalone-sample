52c5ba398e5be048b67eb9d09b94787eae0a6267
naga <naga@area-b.com>
2016-10-15 10:02:04 +0900

    はじめてのSpring Boot 改訂版 JdbcTemplate を使ってDBアクセス

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (first_name, last_name) values ('Nobita', 'Nobi');
+INSERT INTO customers (first_name, last_name) values ('Takeshi', 'Goda');
+INSERT INTO customers (first_name, last_name) values ('Suneo', 'Honekawa');
+INSERT INTO customers (first_name, last_name) values ('Shizuka', 'Minamoto');
